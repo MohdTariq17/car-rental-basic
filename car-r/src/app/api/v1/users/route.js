12d8366e3228d8a4e2ecb6b8539415725bfe012c
MohdTariq17 <mohdtariq70971@gmail.com>
2025-09-01 00:51:17 -0700

    Creating route files only (no code in them)

---
+++ car-r/src/app/api/v1/users/route.js
@@ -0,0 +1,8 @@
+import {PrismaClient} from'../../../../../generated/prisma-client';
+
+//generate api curd routes for users as per prisma users model
+//npm i bcrypt
+//npm i bcryptjs
+//postapi
+
+
